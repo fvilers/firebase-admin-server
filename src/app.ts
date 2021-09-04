c0fdf0e172bac7af67e1c3bf9f05f4d8630cfe5b
Fabian Vilers <fabian.vilers@dev-one.com>
2021-09-04 10:44:50 +0200

    Add a request logger

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import logger from "morgan";
 
 const app = express();
 
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
