8a0b91ee267befd0cf82650b0613441128c3e106
Fabian Vilers <fabian.vilers@dev-one.com>
2021-09-04 20:29:19 +0200

    Add the cors middleware

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ A Web Application that acts as a wrapper around the Firebase Admin SDK
 
 ## Environment variables
 
+- CORS_ORIGIN: the Access-Control-Allow-Origin CORS header
 - GOOGLE_APPLICATION_CREDENTIALS: path to the service account private key
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
+import cors from "cors";
+import dotenv from "dotenv";
 import express from "express";
 import logger from "morgan";
 import errorHandler from "./middlewares/error-handler";
 import notFound from "./middlewares/not-found";
 import routes from "./routes";
 
+dotenv.config();
+
 const app = express();
 
+app.use(cors({ origin: process.env.CORS_ORIGIN }));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
