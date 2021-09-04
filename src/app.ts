782f7a90ffd2d45ea4fcc277f32a7bc775f2413e
Fabian Vilers <fabian.vilers@dev-one.com>
2021-09-04 10:45:59 +0200

    Add the not found middleware

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import logger from "morgan";
+import notFound from "./middlewares/not-found";
 
 const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// TODO: add routing here
+
+app.use("*", notFound());
 
 export default app;
